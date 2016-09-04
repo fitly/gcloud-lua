f8bc94703d8b8bc23b461a7cd676ddf39c176836
christopher5106 <christopher.bourez@gmail.com>
2016-09-04 18:10:40 +0200

    add token refresh

--- gcloud/init.lua
+++ gcloud/init.lua
@@ -14,36 +14,59 @@ M = {}
 
 
 function M.from_service_account_json (json_file)
+  if not path.exists(json_file) then print("JSON config file not found.") ; return false end
   file = io.open(json_file, "r")
   io.input(file)
-  value = cjson.decode(io.read("*a"))
+  M.config = cjson.decode(io.read("*a"))
+  print("Gcloud config file loaded.")
+end
 
+function M.get_token()
+  if not M.config then return print([[Initialize the SDK with command :
+
+    client.from_service_account_json("key.json")
+
+     ]]) end
   local header = cjson.encode{alg="RS256",typ="JWT"}
-  local claim_set = cjson.encode{aud=value.token_uri , exp=(os.time()+10), iat= os.time(),iss=value.client_email, scope="https://www.googleapis.com/auth/devstorage.read_write https://www.googleapis.com/auth/cloud-platform https://www.googleapis.com/auth/datastore https://www.googleapis.com/auth/datastore"}
+  local current_time = os.time()
+  local claim_set = cjson.encode{aud=M.config.token_uri , exp=(os.time()+10), iat= current_time,iss=M.config.client_email, scope="https://www.googleapis.com/auth/devstorage.read_write https://www.googleapis.com/auth/cloud-platform https://www.googleapis.com/auth/datastore https://www.googleapis.com/auth/datastore"}
   local input_signature = base64.encode(header) .. "." .. base64.encode (claim_set)
-  local kpriv = assert(crypto.pkey.from_pem(value.private_key, true))
+  local kpriv = assert(crypto.pkey.from_pem(M.config.private_key, true))
   local signature = input_signature .. "." .. base64.encode(crypto.sign('sha256', input_signature, kpriv))
-
 
   local s = "grant_type=" .. urlencode.string("urn:ietf:params:oauth:grant-type:jwt-bearer") .. "&assertion=" .. urlencode.string(signature)
 
-  local body, code, headers, status = https.request(value.token_uri,s)
+  local body, code, headers, status = https.request(M.config.token_uri,s)
   -- print(body, code, headers, status)
   results = cjson.decode(body)
   if code == 200 then
-    M.access_token = results.access_token
-    M.expires_in = results.expires_in
+    M.auth = {}
+    M.auth.access_token = results.access_token
+    M.auth.valid_until = results.expires_in + os.time()
   end
-  M.project_id = value.project_id
+end
+
+function M.check_valid_token()
+  if not M.auth or os.time() > M.auth.valid_until then
+    print("Refreshing token.")
+    M.get_token()
+  end
+  if M.auth.valid_until and os.time() < M.auth.valid_until then
+    print("Authenticated.")
+    return true
+  end
+  print("No able to authenticate.")
+  return false
 end
 
 function M.upload_from_string(s, bucket, dest_filepath)
+  if not M.check_valid_token() then return nil end
   local url = upload_url .. bucket .. "/o?uploadType=media"
   if dest_filepath ~= nil then url = url .. "&name=" .. dest_filepath end
   local body, code, headers, status = https.request{
     url = url,
     headers = {
-      ["Authorization"] = "Bearer " .. M.access_token ,
+      ["Authorization"] = "Bearer " .. M.auth.access_token ,
       ["Content-length"] = #s,
       ["Content-Type"] =  "multipart/form-data",
     },
@@ -55,6 +78,7 @@ function M.upload_from_string(s, bucket, dest_filepath)
 end
 
 function M.upload(filepath, bucket, dest_filepath)
+  if not M.check_valid_token() then return nil end
   local fileHandle = io.open( filepath)
   local len = fileHandle:seek( "end", 0 )
 	io.close(fileHandle)
@@ -63,7 +87,7 @@ function M.upload(filepath, bucket, dest_filepath)
   local body, code, headers, status = https.request{
     url = url,
     headers = {
-      ["Authorization"] = "Bearer " .. M.access_token ,
+      ["Authorization"] = "Bearer " .. M.auth.access_token ,
       ["Content-Type"] =  "multipart/form-data",
       ["Content-Length"] = len
     },
@@ -75,12 +99,13 @@ function M.upload(filepath, bucket, dest_filepath)
 end
 
 function M.download_as_string(bucket, filepath)
+  if not M.check_valid_token() then return nil end
   local url = download_url .. bucket .. "/o/" .. urlencode.string(filepath) .. "?alt=media"
   local t = {}
   local body, code, headers, status = https.request{
     url = url,
     headers = {
-      ["Authorization"] = "Bearer " .. M.access_token ,
+      ["Authorization"] = "Bearer " .. M.auth.access_token ,
     },
     method = "GET",
     sink = ltn12.sink.table(t)
@@ -90,12 +115,13 @@ function M.download_as_string(bucket, filepath)
 end
 
 function M.download(bucket, filepath, file)
+  if not M.check_valid_token() then return nil end
   local url = download_url .. bucket .. "/o/" .. urlencode.string(filepath) .. "?alt=media"
   local t = {}
   local body, code, headers, status = https.request{
     url = url,
     headers = {
-      ["Authorization"] = "Bearer " .. M.access_token ,
+      ["Authorization"] = "Bearer " .. M.auth.access_token ,
     },
     method = "GET",
     sink = ltn12.sink.file(io.open(file, "w"))
@@ -105,7 +131,8 @@ function M.download(bucket, filepath, file)
 end
 
 function M.insert(kind, properties)
-  local url = datastore_url .. M.project_id .. ":commit"
+  if not M.check_valid_token() then return nil end
+  local url = datastore_url .. M.config.project_id .. ":commit"
 
   local t = {}
   local s = cjson.encode{
@@ -124,7 +151,7 @@ function M.insert(kind, properties)
   local body, code, headers, status = https.request{
     url = url,
     headers = {
-      ["Authorization"] = "Bearer " .. M.access_token ,
+      ["Authorization"] = "Bearer " .. M.auth.access_token ,
       ["Content-length"] = #s,
       ["Content-Type"] =  "multipart/form-data",
     },
